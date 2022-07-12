8b5d6cbb0fb1ab757a3f4a88c6b716a39548666a
diabm1 <diabm1855@gmail.com>
2022-07-12 00:35:49 -0400

    added viewEmployeesByManager

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ function loadMainPrompts() {
 // THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
 function viewEmployees() {
   db.findAllEmployees().then(function ([response]) {
-    console.log("\n")
+    console.log("\n");
     console.table(response);
     init();
   });
@@ -149,8 +149,37 @@ function viewEmployeesByDepartment() {
   });
 }
 
-function viewEmployeesByManager(){
-    
+function viewEmployeesByManager() {
+  db.findAllEmployees().then(([response]) => {
+    const managerChoices = response.map(({ id, first_name, last_name }) => ({
+      name: `${first_name} ${last_name}`,
+      value: id,
+    }));
+
+    prompt([
+      {
+        type: "list",
+        name: "managerId",
+        message: "Which employee do you want to see reports for?",
+        choices: managerChoices,
+      },
+    ])
+      .then((res) => db.findEmployeesByManager(res.managerId))
+      .then(([rows]) => {
+        let employees = rows;
+        console.log("\n");
+        if (employees.length === 0) {
+          console.log("The selected employeehas no reports");
+        }
+        console.table(employees);
+      })
+      .then(() => init());
+  });
+}
+
+function removeEmployee(){
+    db.findAllEmployees()
+    .then(([rows]))
 }
 
 // WHEN I choose to view all roles
@@ -162,6 +191,8 @@ function viewRoles() {
   });
 }
 
+// WHEN I choose to add an employee
+// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 async function addEmployee() {
   const [roles] = await DB.getRoles();
   console.log(roles);
@@ -203,18 +234,13 @@ async function addEmployee() {
 // WHEN I choose to view all departments
 // THEN I am presented with a formatted table showing department names and department ids
 
-
-
-
-
 // WHEN I choose to add a department
 // THEN I am prompted to enter the name of the department and that department is added to the database
 
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
 
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
+
 
 // WHEN I choose to update an employee role
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
